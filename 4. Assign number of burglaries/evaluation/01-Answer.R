513fb8fdab9a7957838987fe9d4c29b35e861d66
Kural <kuralarasan.kumar@ugent.be>
2025-06-22 13:46:24 +0200

    update

--- 4. Assign number of burglaries/evaluation/01-Answer.R
+++ 4. Assign number of burglaries/evaluation/01-Answer.R
@@ -22,29 +22,6 @@ context({
       }
     )
   })
-  
-  testcase("Check if variable was created using assignment operator", {
-    testEqual(
-      "Code used for assignment",
-      function(env) { 
-        # This is just a placeholder since we can't directly check the code used
-        # The real test happens in the comparator
-        TRUE
-      },
-      TRUE,
-      comparator = function(got, want, ...) {
-        # Get the submitted code
-        code <- toString(get_student_code())
-        
-        # Check if the code contains the assignment operator
-        if (!grepl("burglaries\\s*<-\\s*146", code) && !grepl("burglaries\\s*=\\s*146", code)) {
-          get_reporter()$add_message("⚠️ Make sure you're using the assignment operator '<-' or '=' to assign the value 146 to the variable 'burglaries'.", type = "warning")
-          # We don't fail the test, just provide a warning
-        }
-        return(TRUE)
-      }
-    )
-  })
 }, preExec = {
   # No setup needed for this simple exercise
 })
