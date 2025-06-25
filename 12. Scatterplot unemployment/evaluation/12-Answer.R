0cdb2453763493dea1e21cea231f0569be6a0315
Kural <kuralarasan.kumar@ugent.be>
2025-06-25 22:19:30 +0200

    refactor: move data initialization outside of context for improved clarity

--- 12. Scatterplot unemployment/evaluation/12-Answer.R
+++ 12. Scatterplot unemployment/evaluation/12-Answer.R
@@ -1,3 +1,6 @@
+werkloosheid <- c(4.2, 5.7, 7.9, 6.5, 8.1, 5.3, 9.2, 7.1, 6.8, 5.9, 8.5, 7.4, 6.2, 9.0, 5.1)
+criminaliteitscijfers <- c(25.3, 28.6, 33.1, 29.7, 35.4, 26.8, 38.2, 31.5, 30.9, 29.3, 36.1, 32.7, 29.0, 37.8, 27.2)
+
 context({
   testcase("Feedback bij spreidingsdiagram werkloosheid en criminaliteit", {
     testEqual(
@@ -40,5 +43,3 @@ context({
   werkloosheid <- c(4.2, 5.7, 7.9, 6.5, 8.1, 5.3, 9.2, 7.1, 6.8, 5.9, 8.5, 7.4, 6.2, 9.0, 5.1)
   criminaliteitscijfers <- c(25.3, 28.6, 33.1, 29.7, 35.4, 26.8, 38.2, 31.5, 30.9, 29.3, 36.1, 32.7, 29.0, 37.8, 27.2)
 })
-
-plot(werkloosheid, criminaliteitscijfers, main = "Relatie tussen werkloosheid en criminaliteit", xlab = "Werkloosheidspercentage (%)", ylab = "Criminaliteitscijfer (per 1.000)")
