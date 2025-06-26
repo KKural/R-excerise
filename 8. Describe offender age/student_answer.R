eaf1a477bfbd8f03518ada049ceed1f313bfa2d2
Kural <kuralarasan.kumar@ugent.be>
2025-06-26 19:24:29 +0200

    refactor: remove student_answer.R file and its age summary implementation

--- 8. Describe offender age/student_answer.R
+++
@@ -1,6 +0,0 @@
-# Vector met leeftijden van verdachten
-leeftijden_daders <- c(19, 23, 45, 32, 28, 21, 24, 19, 37, 42, 18, 25, 22, 31, 19, 
-                        27, 36, 29, 24, 33, 41, 26, 20, 38, 22, 19, 25, 29, 31, 34)
-
-# Genereer een samenvatting van de leeftijden
-leeftijd_samenvatting <- summary(leeftijden_daders)
