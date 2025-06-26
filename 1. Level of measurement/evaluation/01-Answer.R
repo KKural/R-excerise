5d71d6a97a08839e63ee19968b30712627570d88
KKural <122997366+KKural@users.noreply.github.com>
2025-06-26 12:20:22 +0200

    Update 01-Answer.R

--- 1. Level of measurement/evaluation/01-Answer.R
+++ 1. Level of measurement/evaluation/01-Answer.R
@@ -7,20 +7,18 @@ context({
     testEqual(
       "",
       function(env) {
-        # We'll simply check if there's use of str() function on df_crime_data in the code
+        # Get all objects in the environment
+        all_expressions <- ls(envir = env)
         
-        # Get the submitted code
-        student_code <- readLines("student.R")
+        # Check if the student code contains a call to str(df_crime_data)
+        student_code <- deparse(body(sys.function()))
+        code_analysis <- any(grepl("str\\(df_crime_data\\)", student_code))
         
-        # Join all lines
-        student_code_text <- paste(student_code, collapse = "\n")
-        
-        # Check for str(df_crime_data) in the code
-        # Allow for whitespace around the parentheses and function name
-        if (grepl("str\\s*\\(\\s*df_crime_data\\s*\\)", student_code_text)) {
+        # Create a custom check result
+        if (code_analysis) {
           return(TRUE)
         } else {
-          return(FALSE)
+          return(FALSE) 
         }
       },
       TRUE,
@@ -38,18 +36,13 @@ context({
           type = "success"
         )
         
-        get_reporter()$add_message(
-          "Output van str(df_crime_data):",
-          type = "info"
-        )
-        
+        # Show the output of str(df_crime_data)
+        get_reporter()$add_message("Output van str(df_crime_data):", type = "info")
         str_output <- capture.output(str(df_crime_data))
         get_reporter()$add_message(paste(str_output, collapse = "\n"), type = "code")
         
-        get_reporter()$add_message(
-          "Je kunt ook deze functies gebruiken om data te verkennen:",
-          type = "info"
-        )
+        # Show other useful functions
+        get_reporter()$add_message("Je kunt ook deze functies gebruiken:", type = "info")
         
         get_reporter()$add_message("names(df_crime_data):", type = "info")
         names_output <- capture.output(print(names(df_crime_data)))
@@ -64,14 +57,13 @@ context({
     )
   })
 }, preExec = {
-  # Create the test data frame that will be available to student code
+  # Create crime data in the preExec block as in the example
   df_crime_data <- data.frame(
-    zaak_id = sprintf("ZAAK%03d", 1:10),
-    datum = as.Date("2023-01-01") + 0:9,
-    district = c("Noord", "Zuid", "Oost", "West", "Centrum", "Noord", "Zuid", "Oost", "West", "Centrum"),
-    delict = c("Diefstal", "Aanval", "Inbraak", "Vandalisme", "Diefstal", "Aanval", "Inbraak", "Vandalisme", "Diefstal", "Aanval"),
-    waardeverlies = c(35, 42, 18, 27, 53, 21, 45, 33, 19, 30),
-    agenten = c(2, 3, 1, 2, 4, 2, 3, 2, 1, 3),
-    reactietijd = c(12.5, 8.3, 15.2, 10.1, 7.5, 13.8, 9.2, 11.5, 14.3, 8.7)
+    zaak_id = c("Z001", "Z002", "Z003", "Z004", "Z005"),
+    datum = as.Date(c("2023-01-15", "2023-01-20", "2023-02-05", "2023-02-10", "2023-03-01")),
+    district = c("Noord", "Centrum", "Zuid", "Oost", "West"),
+    delict = c("Diefstal", "Inbraak", "Vandalisme", "Fraude", "Aanranding"),
+    aantal_agenten = c(2, 3, 1, 2, 4),
+    reactietijd_min = c(15.2, 8.7, 12.3, 20.5, 10.8)
   )
 })
